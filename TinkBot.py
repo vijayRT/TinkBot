f8b3e29c5eb2b0ee48cc2a283df23fc389d6985d
Kronosfear <vijayrajasekar1@gmail.com>
2016-12-17 03:47:10 +0530

    Kappa commit

--- TinkBot.py
+++ TinkBot.py
@@ -14,5 +14,7 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!links'):
         await client.send_message(message.channel, 'Check out the useful-links channel to the left for all the basic resources needed for Bot programming.')
+    elif message.content.startswith('!kappa'):
+        await client.send_message(message.channel, 'Kappa raised')
 
 client.run('MjU4OTYwNzU1NjA3NjAxMTUy.CzRBqA.fsBJYI9ZFzrlmJP57FYR6FdDifA')
