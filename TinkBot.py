85dd738b925a077ccbfceec032b0a5c989955d77
Kronosfear <vijayrajasekar1@gmail.com>
2016-12-16 16:29:51 +0530

    Initial commit

---
+++ TinkBot.py
@@ -0,0 +1,18 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!kappa'):
+        await client.send_message(message.channel, 'Kappa raised')
+
+client.run('MjU4OTYwNzU1NjA3NjAxMTUy.CzRBqA.fsBJYI9ZFzrlmJP57FYR6FdDifA')
